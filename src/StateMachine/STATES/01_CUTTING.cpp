c1d7c047ec57d535e3b17658532b39925d46ce06
akivaweil <akivaweil@gmail.com>
2025-07-13 22:51:46 -0700

    Before fixing cut motor phase reset issue

--- src/StateMachine/STATES/01_CUTTING.cpp
+++ src/StateMachine/STATES/01_CUTTING.cpp
@@ -56,7 +56,7 @@ void updateCuttingState() {
         Serial.println("Cut motor backward movement COMPLETE");
         
         // Cutting sequence complete, transition to feeding
-        transitionToState(STATE_FEEDING);
+        transitionToState(STATE_FEEDING);3
       }
       break;
   }
